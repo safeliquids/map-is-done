f43f4f65c9a8850cc12de0689532e9860fed670d
safeliquids <neumetel.kovovy@gmail.com>
2023-10-14 14:48:23 +0200

    move initial analysis of config dict to separate function

--- mid.py
+++ mid.py
@@ -57,43 +57,30 @@ class Registry:
         print(f"registered action of type `{action_type}'")
 
 
-if __name__ == "__main__":
+def extract_config(raw_config: dict) -> dict:
+    """Extracts only relevant fields from configuration.
+    Also verifies that all required fields are present."""
     config = {}
-    properties = {
-        "actions":{"required":True, "found":False},
-        "description":{"required":False, "found":False}
-    }
+    for f, t in zip(["world", "output_directory", "actions"], [str, str, list]):
+        field = raw_config.get(f)
+        if field is None or not isinstance(field, t):
+            raise ValueError(f"config field {f} is missing or of invalid type")
+        config[f] = field
+    return config
+
+
+if __name__ == "__main__":
     with open("example.json", "r", encoding="utf-8") as conf_file:
         raw_config = json.load(conf_file)
+    config = extract_config(raw_config)
     
-    for k, v in raw_config.items():
-        if k not in properties:
-            print(f"found unknown property `{k}', ignoring.")
-            continue
-        
-        property_info = properties[k]
-        if property_info["found"]:
-            print(f"property `{k}' was given multiple times. stopping.")
-            exit(-1)
-
-        if property_info["required"]:
-            print(f"found required property `{k}' in file")
-        else:
-            print(f"found optional property `{k}' in file")
-        
-        config[k] = v
-        property_info["found"] = True
-    
-    for name, info in properties.items():
-        if info["required"] and name not in config:
-            print(f"missing required property `{name}'. stopping")
-            exit(-1)
-    
+    print("=" * 40)
     print("resulting config:")
-    print(json.dumps(config))
+    print(json.dumps(config, indent=4))
     print("=" * 40)
 
     reg = Registry()
     for action in config["actions"]:
         reg.register(action)
+    print("=" * 40)
     
